4cb33b47cc98ed4bf20ec4430cc3691d14b09bb0
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2011-08-08 23:50:27 +0900

    Generate build/subst by configure

--- build/autogen.sh
+++ build/autogen.sh
@@ -8,5 +8,12 @@ rm -rf autom4te*.cache
 autoheader
 autoconf
 
+cp build/subst.in.pl build/subst.in
+sed -n \
+  -e "s/^/ /;s/$/ /;s/'/ ' /" \
+  -e "/^ ac_subst_vars=/,/'/s/^.* \([A-Za-z_][A-Za-z0-9_]*\) .*/\1=@\1@/p" \
+  configure \
+  >>build/subst.in
+
 rm -rf autom4te*.cache
 
